6b0ea7412decdc471c9252e266fccf2dc716eb22
Jeff Minard <jeff@jrm.cc>
2017-02-28 19:44:13 -0800

    Return the demo app to something generic

--- demo/demo.go
+++ demo/demo.go
@@ -1,50 +1,34 @@
 package main
 
 /**
- * A simple demo app that will listen on 8001/8002 and report on recieved events
+ * A simple demo app that will listen on argv[1] and report on recieved events
  *
- *     go run demo.go
+ *     go run demo.go 8001
  */
 
 import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
 )
-
-func getEvent(w http.ResponseWriter, r *http.Request, port int) {
-	body, _ := ioutil.ReadAll(r.Body)
-	log.WithFields(log.Fields{
-		"len":     r.ContentLength,
-		"content": string(body),
-		"id":      r.Header.Get("X-Capacitor-Id"),
-		"port":    port,
-	}).Info("got-event")
-	w.Write([]byte("Thanks!"))
-}
-
-type srv8001 struct{}
-
-func (m *srv8001) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	getEvent(w, r, 8001)
-}
-
-type srv8002 struct{}
-
-func (m *srv8002) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	getEvent(w, r, 8002)
-}
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: time.RFC3339Nano,
 	})
-	log.Info("starting server")
-	go func() {
-		http.ListenAndServe(":8001", &srv8001{})
-	}()
-	http.ListenAndServe(":8002", &srv8002{})
+	log.Info("Server starting @ port "+os.Args[1])
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		log.WithFields(log.Fields{
+			"len":     r.ContentLength,
+			"content": string(body),
+			"id":      r.Header.Get("X-Capacitor-Id"),
+		}).Info("event")
+		w.Write([]byte("Thanks!"))
+	})
+	http.ListenAndServe(":"+os.Args[1], nil)
 }
