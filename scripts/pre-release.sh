e972fd2d787dd3820c1180c1facfb21e06fc24db
Jeff Minard <jeff@jrm.cc>
2017-03-01 20:14:40 -0800

    Maybe this variant will work..

--- scripts/pre-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-go get github.com/mitchellh/gox
-
-go get github.com/tcnksm/ghr
-
-gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-
-ghr --username chuyskywalker --token $GITHUB_TOKEN --replace --prerelease --debug pre-release dist/
